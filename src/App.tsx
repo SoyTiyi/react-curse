c1faefd1d6d24729dc1781d9b162f1b6ae37b6c9
Santiago Martínez Martínez <46984191+SoyTiyi@users.noreply.github.com>
2026-01-02 16:16:44 -0500

    useEffect class

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,71 @@
 import "./index.css";
 import { Button } from "./components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function App() {
-  const [count, setCount] = useState(0);
-  const countMore = () => {
-    setCount(count + 1);
+  // Hace un hook (enganchar) el estado del componente
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      setError('Failed to fetch data');
+    }
   };
+
+  // cuando queremos traer data del backend lo que debemos
+  // es comunicarnos a traves de un endpoint
+
+  // Maneja el ciclo de vida de un componente - (Como todos lo explican)
+
+  // Explicacion del profe:
+  // el uso correcto que se le da al useEffect es para sync con
+  // entidades externas al componente
+  // Ejemplo:
+  // 1. Operaciones async
+  // 2. Parametros de entrada (props) vienen del padre
+  // 3. Comunicarnos con APIs
+  // 4. Cuando trabajas con un context
+  useEffect(() => {
+    //Que debe de haber en este metodo?
+    // Aca hay logica, cuando se ejecuta esta logica???
+    // 1. Se ejecuta cuando se monta el componente
+    // 2. Se ejecuta cada vez que se modifique uno de los valores del state
+    // que este en el arreglo de dependencias, si yo no pongo el arreglo de dependencias
+    // hace que esta logica se ejecute cada vez que haya un cambio en el estado del componente
+    // 3.
+    fetchData();
+    // si pones un return dentro del useEffect, ese return se ejecuta cuando
+    // el componente se va a desmontar o muera
+    // generalmente se usa para hacer limpieza de recursos y liberar memoria
+
+    // return () => {
+    //   console.log("El componente se va a desmontar");
+    // };
+
+    //ademas de la funcion tambien acepta un arreglo de dependencias
+  }, []);
+
+  //Hay veces que se pueden usar mas de dos useEffect en un mismo componente
+  //Tener uno para montar, uno para cambiar data, etc
 
   return (
     <>
-      <Button label={`Count: is ${count}`} parentMethod={countMore} />
+      <div>
+        {data.map((item: any) => (
+  
+          <div key={item.id} className="p-4 m-2 border rounded shadow">
+            <h3 className="font-bold text-lg">{item.title}</h3>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
